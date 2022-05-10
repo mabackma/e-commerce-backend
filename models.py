0971bc50d64a0af37c7a0dfe63534f6c88d7a5bd
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-05-11 02:32:24 +0300

    models.py aloitettu

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
+import pymongo
+from pymongo.server_api import ServerApi
+from config import Config
+from bson.objectid import ObjectId
+from errors.not_found import NotFound
+from errors.validation_error import ValidationError
+
 client = pymongo.MongoClient(
     Config.CONNECTION_STRING,
     server_api=ServerApi('1'))
